2182a3a37ef4f2b801f891c6b29acb8d98e8f213
AnshuPathak-88825 <shubhanshupathak88825@gmail.com>
2023-08-02 13:26:15 +0530

    Created all controller type request for product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,6 +7,69 @@ module.exports.createProduct = async (req, res, next) => {
     product,
   });
 };
-module.exports.getAllProducts = (req, res) => {
-  res.status(200).json({ message: "This is working" });
+// get product
+module.exports.getAllProducts = async (req, res) => {
+  const product = await Product.find();
+  res.status(200).json({ status: true, product });
 };
+
+
+//update product
+module.exports.updateProduct = async (req, res) => {
+
+  try {
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+    res.status(200).json({ status: true, product });
+  } catch (err) {
+    console.error("Error while deleting product:", err);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+ 
+};
+
+//Delete product
+module.exports.deleteProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+    await Product.deleteOne({ _id: req.params.id });
+    res.status(200).json({ status: true, message: "product deleted successfully" });
+  } catch (err) {
+    console.error("Error while deleting product:", err);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+
+};
+
+//Get product Detail
+module.exports.GetProductDetail=async(req,res,next)=>{
+  try {
+    const product=await Product.findById(req.params.id);
+    if(!product)
+    {
+      return res
+      .status(404)
+      .json({ success: false, message: "Product not found" });
+    }
+
+    res.status(200).json({ status: true,product });
+  } catch (err) {
+    console.error("Error while deleting product:", err);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+}
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const productControllor = require("../controllers/productController");
-router.route("/products").get(productControllor.getAllProducts);
-router.route("/products/new").post(productControllor.createProduct);
+const {getAllProducts,createProduct,updateProduct,deleteProduct,GetProductDetail} =require("../controllers/productController")
+router.route("/products").get(getAllProducts);
+router.route("/products/new").post(createProduct);
+router.route("/products/:id").put(updateProduct);
+router.route("/products/:id").delete(deleteProduct);
+router.route("/products/:id").get(GetProductDetail);
+
+
 
 module.exports = router;
