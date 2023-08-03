eee87ac0a80fc3f325259da60676162665ab3f7d
AnshuPathak-88825 <shubhanshupathak88825@gmail.com>
2023-08-03 21:25:07 +0530

    Solved issue of uncaughtException error

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
-const errormiddleware=require("./middleware/error")
+process.on("uncaughtException", (error) => {
+  console.log(`Error: ${error.message}`);
+  console.log("shutting down the server due uncaughtException");
+  process.exit(1);
+});
+const errormiddleware = require("./middleware/error");
 app.use(express.json());
 const product = require("./routes/productRoute");
 app.use("/api/vi", product);
