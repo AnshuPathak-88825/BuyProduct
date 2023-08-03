a02ea0bcf09e3da8b37f09a3f13ccee907a95ff6
AnshuPathak-88825 <shubhanshupathak88825@gmail.com>
2023-08-03 21:38:04 +0530

    Fixed issue for wrong invalid mongoose id

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -2,5 +2,10 @@ const errorhandler = require("../utils/errorhandler");
 module.exports = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.message = error.message || "internal server error";
+  if(error.name==="CastError")
+  {
+    const message=`Resource not found. ${error.path}`;
+    return res.status(error.statusCode).json({ success: false, message: message });
+  }
   res.status(error.statusCode).json({ success: false, message: error.message });
 };
