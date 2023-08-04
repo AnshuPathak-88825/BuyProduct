0daf9b161934244c11484dbe56d4a3122d503b63
AnshuPathak-88825 <shubhanshupathak88825@gmail.com>
2023-08-04 22:14:44 +0530

    Added filter by price

--- backend/utils/Apifeature.js
+++ backend/utils/Apifeature.js
@@ -1,28 +1,32 @@
-class ApiFeature
-{
-    constructor(query,querystr)
-    {
-        this.query=query;
-        this.querystr=querystr;
-    }
-    search()
-    {
-        const keyword=this.querystr.keyword?{name:{
-            $regex:this.querystr.keyword,
-            $options:"i",
-        }}:{};
-        this.query=this.query.find({...keyword});
-        return this;
-    }
-    filter()
-    {
-        // copy value of querystr not passing referrence
-        const queryCopy={...this.querystr};
-        const removeField=["keyword","page","limit"];
-        removeField.forEach((key)=> {delete queryCopy[key]});
-        this.query=this.query.find({...queryCopy});
-        return this;
-    }
-
+class ApiFeature {
+  constructor(query, querystr) {
+    this.query = query;
+    this.querystr = querystr;
+  }
+  search() {
+    const keyword = this.querystr.keyword
+      ? {
+          name: {
+            $regex: this.querystr.keyword,
+            $options: "i",
+          },
+        }
+      : {};
+    this.query = this.query.find(keyword);
+    return this;
+  }
+  filter() {
+    // copy value of querystr not passing referrence
+    const queryCopy = { ...this.querystr };
+    const removeField = ["keyword", "page", "limit"];
+    removeField.forEach((key) => {
+      delete queryCopy[key];
+    });
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+ 
 }
-module.exports=ApiFeature;
+module.exports = ApiFeature;
