7b2ad0bcb795cfccf61ec21410c093efcdc16ffd
AnshuPathak-88825 <shubhanshupathak88825@gmail.com>
2023-08-10 13:47:40 +0530

    Only admin can make changes in product this functionality has been  devleoped

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,15 +1,26 @@
 const Errorhandler = require("../utils/errorhandler");
 const catchAsyncError = require("./catchAsyncError");
-const jwt=require("jsonwebtoken");
-const User=require("../model/userModel")
-exports.isAuthenticatedUser=catchAsyncError(async(req,res,next)=>{
-    const {token}=req.cookies;
-    if(!token)
-    {
-        return next(new Errorhandler("Please to login to access",401));
-    }
-    const decodedData=jwt.verify(token,process.env.JWT_SECRET);
-    req.user=await User.findById(decodedData._id); 
-    next();
-
+const jwt = require("jsonwebtoken");
+const User = require("../model/userModel");
+exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return next(new Errorhandler("Please to login to access", 401));
+  }
+  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = await User.findById(decodedData.id);
+  next();
 });
+exports.authorizeRoles = (...role) => {
+  // console.log(await (req.user))
+  return (req, res, next) => {
+    if (!role.includes(req.user.role)) {
+      return next(
+        new Errorhandler(
+          `Role: ${req.user.role} is not allowed to access this resource `
+        )
+      );
+    }
+    next();
+  };
+};
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const productControllor = require("../controllers/productController");
-const {isAuthenticatedUser}=require("../middleware/auth");
+const {isAuthenticatedUser,authorizeRoles}=require("../middleware/auth");
 const {getAllProducts,createProduct,updateProduct,deleteProduct,GetProductDetail} =require("../controllers/productController")
-router.route("/products").get(isAuthenticatedUser,getAllProducts);
-router.route("/products/new").post(isAuthenticatedUser,createProduct);
-router.route("/products/:id").put(isAuthenticatedUser,updateProduct);
-router.route("/products/:id").delete(isAuthenticatedUser,deleteProduct);
+router.route("/products").get(authorizeRoles("admin"),getAllProducts);
+router.route("/products/new").post(isAuthenticatedUser,authorizeRoles("admin"),createProduct);
+router.route("/products/:id").put(isAuthenticatedUser,authorizeRoles("admin"),updateProduct);
+router.route("/products/:id").delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct);
 router.route("/products/:id").get(GetProductDetail);
 
 
