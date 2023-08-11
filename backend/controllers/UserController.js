88eae55a84647025095ffcc0504a2abce02ac288
AnshuPathak-88825 <shubhanshupathak88825@gmail.com>
2023-08-11 23:56:19 +0530

    Create get all user funality for admin

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -153,3 +153,16 @@ exports.updateUserProfile=catchAsyncErrors(async(req,res,next)=>{
     user
   })
 });
+
+//Get all user(admin)
+exports.GetAllUser=catchAsyncErrors(async(req,res,next)=>{
+  const user=await User.find();
+  res.status(200).json({
+    success:true,
+    user
+  })
+})
+
+
+// //Get single user (admin) 
+// exports.Get
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const router=express.Router();
-const {registerUser,loginUser,logout,forgotPassword,resetPassword, getUserDetails,updatePassword,updateUserProfile}=require("../controllers/UserController");
+const {GetAllUser,registerUser,loginUser,logout,forgotPassword,resetPassword, getUserDetails,updatePassword,updateUserProfile}=require("../controllers/UserController");
 const {isAuthenticatedUser,authorizeRoles}=require("../middleware/auth");
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
@@ -10,5 +10,6 @@ router.route("/logout").get(logout);
 router.route("/me").get(isAuthenticatedUser,getUserDetails);
 router.route("/me/update").put(isAuthenticatedUser,updateUserProfile);
 router.route("/password/update").get(isAuthenticatedUser,updatePassword);
+router.route("/admin/users").get(isAuthenticatedUser,authorizeRoles("admin"),GetAllUser);
 
 module.exports=router;
