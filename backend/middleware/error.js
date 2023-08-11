19ac87cb1dd3b987d957022bc47c37a1d8629d3d
AnshuPathak-88825 <shubhanshupathak88825@gmail.com>
2023-08-11 22:19:40 +0530

    Added error message related to jwt token

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -14,5 +14,17 @@ module.exports = (error, req, res, next) => {
     const message=`Duplicate ${Object.keys(error.keyValue)} entered`
     error=new errorhandler(message,400);
   }
+  //Wrong JWT error
+  if(error.name==="JsonWebTokenError")
+  {
+    const message=`Json web Token is invalid, Try agani`;
+    error=new errorhandler(message,400);
+  }
+  //JWT EXPIRE errir
+  if(error.name==="TokenExpiredError")
+  {
+    const message=`Json web Token is expired, Try again`;
+    error=new errorhandler(message,400);
+  }
   res.status(error.statusCode).json({ success: false, message: error.message });
 };
