91fa0e7ff64502243d2729443be5f612afe8e3d2
AnshuPathak-88825 <shubhanshupathak88825@gmail.com>
2023-08-18 01:47:32 +0530

    Added Loader to while making api call

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -5,31 +5,42 @@ import Product from "./Product";
 import MetaData from "../layout/MetaData";
 import { getProduct } from "../../actions/productAction";
 import { useSelector, useDispatch } from "react-redux";
+// import Loader from "../component/layout/Loader/Loader";
+import Loader from "../layout/Loader/Loader";
+
 
 const Home = () => {
   const dispatch = useDispatch();
- const a=useSelector((state)=>state.products);
+  const a = useSelector((state) => state.products);
   useEffect(() => {
     dispatch(getProduct());
   }, []);
+  console.log(a);
   return (
     <Fragment>
-      <MetaData title="ECommerce"></MetaData>
-      <div className="banner">
-        <p>Welcome to Ecommerce</p>
-        <h1>Find amazing product below</h1>
-        <a href="#container">
-          <button>
-            Scroll <CgMouse />
-          </button>
-        </a>
-      </div>
-      <h2 className="homeHeading">Featured Products</h2>
-      <div className="container" id="container">
-      {a.product&&a.product.map((p,index)=>{
-         return <Product key={index} product={p} />
-      })    }
-      </div>
+      {a.loading ? (
+        <Loader/>
+      ) : (
+        <Fragment>
+          <MetaData title="ECommerce"></MetaData>
+          <div className="banner">
+            <p>Welcome to Ecommerce</p>
+            <h1>Find amazing product below</h1>
+            <a href="#container">
+              <button>
+                Scroll <CgMouse />
+              </button>
+            </a>
+          </div>
+          <h2 className="homeHeading">Featured Products</h2>
+          <div className="container" id="container">
+            {a.product &&
+              a.product.map((p, index) => {
+                return <Product key={index} product={p} />;
+              })}
+          </div>
+        </Fragment>
+      )}
     </Fragment>
   );
 };
